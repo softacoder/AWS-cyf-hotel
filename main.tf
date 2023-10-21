3c77281f77885b7d8767f578cb72268c43405ea0
softacoder <113185323+softacoder@users.noreply.github.com>
2023-10-21 14:55:50 +0100

    commit

--- main.tf
+++ main.tf
@@ -1,24 +1,24 @@
-provider "aws" {
-  region = "ec-west-2"  
-}
+# provider "aws" {
+#   region = "eu-west-2"  
+# }
 
-resource "aws_s3_bucket" "cyf_frontend" {
-  bucket = "cyf-frontend-bucket"  # Choose a unique bucket name
-  acl    = "public-read"
+# resource "aws_s3_bucket" "cyf_frontend" {
+#   bucket = "cyf-frontend-bucket"
+#   acl    = "public-read"
 
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Action = "s3:GetObject",
-        Effect = "Allow",
-        Resource = aws_s3_bucket.cyf_frontend.arn,
-        Principal = "*",
-      },
-    ],
-  })
+#   policy = jsonencode({
+#     Version = "2012-10-17",
+#     Statement = [
+#       {
+#         Action = "s3:GetObject",
+#         Effect = "Allow",
+#         Resource = aws_s3_bucket.cyf_frontend.arn,
+#         Principal = "*",
+#       },
+#     ],
+#   })
 
-  versioning {
-    enabled = true
-  }
-}
+#   versioning {
+#     enabled = true
+#   }
+# }
