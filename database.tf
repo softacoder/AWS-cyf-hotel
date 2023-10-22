ff18ad4067a84e6991010d9af44933203aa8f846
softacoder <113185323+softacoder@users.noreply.github.com>
2023-10-22 09:39:06 +0100

    commit setup hcl code

--- database.tf
+++ database.tf
@@ -1,17 +1,17 @@
 # version 1
-# provider "aws" {
-#   region = "eu-west-2"  
-# }
+provider "aws" {
+  region = "eu-west-2"  
+}
 
-# resource "aws_db_instance" "cyf_hotel_db" {
-#   allocated_storage    = 20
-#   storage_type         = "gp2"
-#   engine               = "postgres"
-#   engine_version       = "13.4"
-#   instance_class       = "db.t2.micro"
-#   name                 = "cyfhotel"
-#   username             = "db_user"
-#   password             = "your_password"
-#   parameter_group_name = "default.postgres13"
-#   skip_final_snapshot  = true
-# }
+resource "aws_db_instance" "cyf_hotel_db" {
+  allocated_storage    = 20
+  storage_type         = "gp2"
+  engine               = "postgres"
+  engine_version       = "13.4"
+  instance_class       = "db.t2.micro"
+  name                 = "cyfhotel"
+  username             = "db_user"
+  password             = "your_password"
+  parameter_group_name = "default.postgres13"
+  skip_final_snapshot  = true
+}
