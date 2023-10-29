2b6101d9353e019cd0800b9c1b53556ac899c78e
softacoder <113185323+softacoder@users.noreply.github.com>
2023-10-29 20:58:31 +0000

    commit update tf files

--- database.tf
+++ database.tf
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:290c6ac40c6109d4a9253bd5f496c25108c87dec60dc39fed00689a4394c46e5
-size 3151
+oid sha256:9eba5032188e2f019a350a7c4f49a5d27ad2d7d53bb7d047903fe5d88dd478a5
+size 3275
--- main.tf
+++ main.tf
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:47546498ebfc0c04737be1c18ce879a1c01960de3fede647da653d41ea69eac6
-size 2904
+oid sha256:e075b0918583f99d7698aef6a64a74b3302b748f61e1b95227d505b3631b3902
+size 3013
