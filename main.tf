7db6b92d2b5f2628dc6719e878141fded001ec9a
softacoder <113185323+softacoder@users.noreply.github.com>
2023-10-30 20:34:19 +0000

    commit update cors code policy

--- main.tf
+++ main.tf
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c3d3b72f75f24ba566620b87203b827bd7a8ef60f325ec7e23d9a1922e3ef177
-size 4867
+oid sha256:1002a8742d8298e8c1daac972d65d3f2ddebc67ef24e02d8c649afb5d0183555
+size 5925
