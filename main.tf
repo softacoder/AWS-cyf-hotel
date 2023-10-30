385cff9c49f4ed1d4c11e9218cfb00572fae5f5b
softacoder <113185323+softacoder@users.noreply.github.com>
2023-10-30 14:18:05 +0000

    commit

--- main.tf
+++ main.tf
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e075b0918583f99d7698aef6a64a74b3302b748f61e1b95227d505b3631b3902
-size 3013
+oid sha256:56aa6831b8dd946a9c44c67599308290a8b7494f3aaf5adc0efce647814c12ff
+size 3077
