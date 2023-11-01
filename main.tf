2b33f5a7740a8c3d0da00f9af396124880427d2a
softacoder <113185323+softacoder@users.noreply.github.com>
2023-11-01 10:34:18 +0000

    commit config.json important

--- main.tf
+++ main.tf
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:16a68d822ef9c73d831c89b4c485b0fc26521f7c2012c4dc5e79a55c736a22de
-size 6081
+oid sha256:f702b489208ff8bbe8eafb5cc88d85980f32a58ecf1952b251f5792b67c3d5c5
+size 6034
